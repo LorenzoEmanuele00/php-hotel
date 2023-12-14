0117f62cf956d755a88687b0081a100d9485a19e
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-12-14 16:10:07 +0100

    refactoring

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $hotels = [
     [
         'name' => 'Hotel Milano',
         'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
+        'parking' => false,
         'vote' => 2,
         'distance_to_center' => 50
     ],
@@ -54,6 +54,37 @@ $hotels = [
 
     <h1>I nostri Hotel</h1>
 
+    <h5>Filter</h5>
+    <?php
+        $park_var = '';
+        $vote_var = '';
+
+        if(isset($_GET['park']) || isset($_GET['vote'])) {
+        $park_var = $_GET['park'];
+        $vote_var = $_GET['vote'];
+        //Process further
+        }
+    ?>
+    <form action="index.php" method="GET" class="d-flex justify-content-center">
+        <select class="form-select" aria-label="Default select example" name="park">
+            <option value="" <?php echo ($park_var === '' ? 'selected="selected"' : '')?>>NO Filter</option>
+            <option value="false" <?php echo ($park_var === 'false' ? 'selected="selected"' : '')?>>NO</option>
+            <option value="true" <?php echo ($park_var === 'true' ? 'selected="selected"' : '')?>>YES</option>
+        </select>
+
+        <select class="form-select" aria-label="Default select example" name="vote">
+            <option value="" <?php echo ($vote_var === '' ? 'selected="selected"' : '')?>>NO Filter</option>
+            <option value="1" <?php echo ($vote_var === '1' ? 'selected="selected"' : '')?>>1</option>
+            <option value="2" <?php echo ($vote_var === '2' ? 'selected="selected"' : '')?>>2</option>
+            <option value="3" <?php echo ($vote_var === '3' ? 'selected="selected"' : '')?>>3</option>
+            <option value="4" <?php echo ($vote_var === '4' ? 'selected="selected"' : '')?>>4</option>
+            <option value="5" <?php echo ($vote_var === '5' ? 'selected="selected"' : '')?>>5</option>
+        </select>
+
+        <button action="index.php">send</button>
+    </form>
+    
+
     <table class="table">
         <thead>
             <tr>
@@ -66,17 +97,19 @@ $hotels = [
         </thead>
         <tbody>
             <?php foreach($hotels as $hotel) { ?>
-                <tr>
-                <td><h6><?php echo $hotel['name']; ?></h6></td>
-                <td><?php echo $hotel['vote']; ?></td>
-                <td><?php echo $hotel['description']; ?></td>
-                <td><?php echo $hotel['distance_to_center']; ?></td>
-                <?php if ($hotel['parking'] === true) { ?>
-                    <td>YES</td>
-                <?php } else { ?>
-                    <td>NO</td>
+                <?php if((empty($_GET['park']) || ($_GET['park'] === 'true') === $hotel['parking']) && (empty($_GET['vote']) || intval($_GET['vote']) === $hotel['vote'])) { ?>
+                    <tr>
+                    <td><h6><?php echo $hotel['name']; ?></h6></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $hotel['distance_to_center']; ?></td>
+                    <?php if ($hotel['parking'] === true) { ?>
+                        <td>YES</td>
+                    <?php } else { ?>
+                        <td>NO</td>
+                    <?php } ?>
+                    </tr>
                 <?php } ?>
-                </tr>
             <?php } ?>
         </tbody>
     </table>
